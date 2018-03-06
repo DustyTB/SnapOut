7d35e0b75df4bebd1f2536b757cb5f3b26901ac1
NeonSpec <xcellghoul@gmail.com>
2018-03-06 22:29:06 +0100

    Fixed bugs

--- Source/WorkGiver_CalmPawnDown.cs
+++ Source/WorkGiver_CalmPawnDown.cs
@@ -8,12 +8,6 @@ namespace Calm_Down
     class WorkGiver_CalmPawnDown : WorkGiver_Warden_Chat
     {
 
-        //Lone boolean 
-        #region lonebool
-        bool aggroCalmEnabled = CDMod.settings.CDAggroCalmEnabled;
-        #endregion
-
-
         //ShouldSkip Thingy
         #region shouldskip
         public override bool ShouldSkip(Pawn pawn)
@@ -28,14 +22,21 @@ namespace Calm_Down
         public override Job JobOnThing(Pawn pawn, Thing thang, bool forced = true)
         {
             Pawn pawn2 = (Pawn)thang;
-            bool shouldDo = false;
-            if (pawn.InAggroMentalState && aggroCalmEnabled) { shouldDo = true; };
-            if (!pawn.InAggroMentalState) { shouldDo = true; };
-            
-            bool recent = Find.TickManager.TicksGame < pawn2.mindState.lastAssignedInteractTime + 15000;
-            if (pawn2.InMentalState && shouldDo && pawn.health.capacities.CapableOf(PawnCapacityDefOf.Talking) && !recent && pawn2.RaceProps.Humanlike && pawn.CanReserve(pawn2)) //Only on mental state pawns
+            if (pawn2.RaceProps.Humanlike)
             {
+                bool ShouldDoFac = (pawn2.IsPrisonerOfColony == CDMod.settings.CDNonFaction);
+                if (pawn2.Faction == Faction.OfPlayer) ShouldDoFac = true;
+
+                bool ShouldDoAgg = (pawn2.InAggroMentalState == CDMod.settings.CDAggroCalmEnabled);
+                if (!pawn2.InAggroMentalState) ShouldDoAgg = true;
+
+
+                bool recent = Find.TickManager.TicksGame < pawn2.mindState.lastAssignedInteractTime + 15000;
+                if (pawn2.InMentalState && ShouldDoFac && ShouldDoAgg && pawn.health.capacities.CapableOf(PawnCapacityDefOf.Talking) && !recent && pawn2.RaceProps.Humanlike && pawn.CanReserve(pawn2)) //Only on non-aggressive mental state pawns
+                {
                     return new Job(CalmDefOf.CalmDown, pawn2);
+                }
+                return null;
             }
             return null;
         }
